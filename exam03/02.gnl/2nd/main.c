a2db3fc309c05933f2428522d7a5c60a5c79f2b1
Jieun Park <jiepark@c1b1c1.42berlin.de>
2025-02-20 13:11:55 +0100

    gnl:ing

--- exam03/02.gnl/2nd/main.c
+++ exam03/02.gnl/2nd/main.c
@@ -5,27 +5,64 @@ I did not copy the actual subject. But it did go along the line
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#define BF_SIZE 1024
 
+int		handle_error(int i, char *s1, char *s2);
+void	*ft_memcopy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+size_t	ft_strlen(char *s);
+char	*ft_strchr(char *s, int c);
+int		str_append_mem(char **s1, char *s2, size_t size2);
+
+int	handle_error(int i, char *s1, char *s2)
+{
+	if (i == 0)
+	{
+		if (s1)
+			free(s1);
+		if (s2)
+			free(s2);
+		fprintf(stderr, "malloc failed");
+		return (1);
+	}
+	else if (i == 1)
+	{
+		perror("read failed\n");
+		return (1);
+	}
+	else if (i == 2)
+	{
+		printf("Error: str_append_mem failed\n");
+		return (1);
+	}
+	return (0);
+}
+//ok
 void	*ft_memcopy(void *dest, const void *src, size_t n)
 {
-	char *d = dest;
-	const char *s = src;
-	int		i;
+	char		*d;
+	const char	*s;
 
+	d = dest;
+	s = src;
 	if (!src || !n)
 		return (NULL);
-	else if (src == dest)
-		return (src);
+	if (s == d)
+		return (d);
 	if (d < s && d + n > s)
-		ft_memmove(d, s, n);
+		ft_memmove(dest, src, n);
 	else
 	{
-		while (--n >= 0)
+		while (n > 0)
+		{
+			n--;
 			d[n] = s[n];
+		}
 	}
-	return d;
+	return (d);
 }
-
 
 //ok
 void	*ft_memmove(void *dest, const void *src, size_t n)
@@ -77,35 +114,115 @@ char	*ft_strchr(char *s, int c)
 	return (NULL);
 }
 
+//append s2 to s1 (concatenate)
+//fixed
+int	str_append_mem(char **s1, char *s2, size_t size2)
+{
+	size_t	size1;
+	char	*tmp;
+
+	size1 = ft_strlen(*s1);
+	tmp = malloc(size2 + size1 + 1);
+	if (!tmp)
+		return (handle_error(0, NULL, NULL));
+	if (*s1)
+		ft_memcopy(tmp, *s1, size1);
+	if (*s2)
+		ft_memcopy(tmp + size1, s2, size2);
+	tmp[size1 + size2] = '\0';
+	*s1 = tmp;
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	char			tmp;
+	char			*buff;
+	static char		*ret;
+	int				new_bytes_read;
+	int				total_read;
+
+//	tmp = ft_strchr(b, '\n');
+	buff = malloc(BF_SIZE + 1);
+	if (!buff)
+	{
+		handle_error(0, buff, NULL);
+		return (NULL);
+	}
+		total_read = 0;
+	while (!tmp)
+	{
+		new_bytes_read = read(fd, buff, BF_SIZE);
+		if (new_bytes_read == -1)
+		{
+			handle_error(1, buff, NULL);
+			return (NULL);
+		}
+		else if (!new_bytes_read)
+			break ;
+		ret = realloc(ret, total_read + new_bytes_read + 1);
+		if (!ret)
+		{
+			handle_error(0, buff, NULL);
+			return (NULL);
+		}
+		if (str_append_mem(ret + total_read, buff, new_bytes_read))
+			exit (handle_error(2, buff, ret));
+		total_read += new_bytes_read;
+		if (ft_strchr(ret, '\n'))
+		{
+			break ;
+			//update stash(ret should be next position of \n)
+			//break;
+		}
+	}
+	buff[total_read] = '\0';
+	return (ret);
+}
+
 int	main(int ac, char **av)
 {
-	char	*s1 = "Hi";
+	int		fd;
+	char	*line;
 
-	s1 = malloc(10);
-	char *s2 = s1 + 10;
-	s2 = "jin";
-	//s1 = "HI";
-	printf("before:[%s]\n", s1);
-	ft_memcpy(s1, s2, 1);
-	printf("after:[%s]\n", s2);
+	if (ac != 2)
+	{
+		printf("argument is not given\n");
+		return (1);
+	}
+	fd = open(av[1], O_RDONLY);
+	if (fd < 0)
+	{
+		perror("open failed\n");
+		exit (1);
+	}
+	while (1)
+	{
+		line = get_next_line(fd);
+		if (!line)
+			break ;
+	}
+	printf("* * *debugging point* * *\n");
 	return (0);
 }
 
 /*
 
-
-int	str_append_mem(char **s1, char *s2, size_t size2)
+int	main()
 {
-	size_t size1 = ft_strlen(*s1);
-	char *tmp = malloc(size2 + size1 + 1);
-	if (!tmp)
-		return 0;
-	ft_memcpy (tmp, *s1, size1);
-	ft_memcpy (tmp + size1, s2, size2);
-	tmp[size1 + size2] = 0;
-	free(*s1);
-	*s1 = tmp;
-	return 1;
+	//check str_append_mem
+	char	*s1;
+	char	*s2;
+	int		res;
+
+	s1 = "hi ";
+	s2 = "jieun ";
+	res = str_append_mem(&s1, s2, 6);
+	if (res)
+		printf("str_append_mem: failed\n");
+	else
+		printf("res : [%s]\n", s1);
+	free(s1);
 }
 
 int	str_append_str(char **s1, char *s2)
@@ -113,26 +230,4 @@ int	str_append_str(char **s1, char *s2)
 	return str_append_mem(s1, s2, ft_strlen(s2));
 }
 
-
-char	*get_next_line(int fd)
-{
-	static char b[BUFFER_SIZE + 1] = "";
-	char *ret = NULL;
-
-	char *tmp = ft_strchr(b, '\n');
-	while (!tmp) {
-		if (!str_append_str(&ret, b))
-			return NULL;
-		int read_ret = read(fd, b, BUFFER_SIZE);
-		if (read_ret == -1)
-			return NULL;
-		b[read_ret] = 0;
-	}
-	if (!str_append_mem(&ret, b, tmp - b + 1))
-	{
-		free(ret);
-		return NULL;
-	}
-	return ret;
-}
 */
--- exam03/02.gnl/note/ft_mem*_unsigned_char.md
+++ exam03/02.gnl/note/ft_mem*_unsigned_char.md
@@ -24,3 +24,55 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 
 
 ```
+
+## how to use unsigned int or size_t in a loop?
+```c
+void	*ft_memcopy(void *dest, const void *src, size_t n)
+{
+	char		*d;
+	const char	*s;
+
+	d = dest;
+	s = src;
+	if (!src || !n)
+		return (NULL);
+	if (s == d)
+		return (d);
+	if (d < s && d + n > s)
+		ft_memmove(dest, src, n);
+	else
+	{
+		while (--n >= 0)
+		{
+			printf("BEFORE: d[%zu]=[%s], s[%zu]=[%s]\n", n, &d[n], n, &s[n]);
+			d[n] = s[n];
+			printf("AFTER: d[%zu]=[%s], s[%zu]=[%s]\n", n, &d[n], n, &s[n]);
+
+		}
+	}
+	return (d);
+}
+
+```
+> this example[1] cases seg fault
+
+#### why segfault?
+-  3 -> 2 -> 1 -> 0 -> 123890128901248904098 very large velue  
+- so change like below  
+
+```c
+void	*ft_memcopy(void *dest, const void *src, size_t n)
+{
+	else
+	{
+		while (n > 0)
+		{
+			n--;
+			printf("BEFORE: d[%zu]=[%s], s[%zu]=[%s]\n", n, &d[n], n, &s[n]);
+			d[n] = s[n];
+			printf("AFTER: d[%zu]=[%s], s[%zu]=[%s]\n", n, &d[n], n, &s[n]);
+		}
+	}
+}
+
+```
